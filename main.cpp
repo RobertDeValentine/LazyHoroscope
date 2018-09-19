5858cf1aa8937ccb913248e7c8bda21922b95cbf
RobertDeValentine <rjdevalentine@mail.csuchico.edu>
2018-09-19 16:12:53 -0700

    asks and collects user's names

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//Authors:
+//Authors: Robert DeValentine
 #include<iostream>
 #include<string>
 
@@ -7,8 +7,14 @@ using namespace std;
 int main()
 {
   //declare variables
+  string first;
+  string last;
 
   //get user input
+  cout<<"What is your first name?: ";
+  cin>>first;
+  cout<<"What is your last name?: ";
+  cin>>last;
 
   //tell fortune
 
