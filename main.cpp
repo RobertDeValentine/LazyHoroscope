d147cf8a30046036ceeab63f121630b317fd9c14
RobertDeValentine <rjdevalentine@mail.csuchico.edu>
2018-09-24 17:08:29 -0700

    Final version of Lazy horriscope in addition to previous tells fortune based on letters in name

--- main.cpp
+++ main.cpp
@@ -12,9 +12,9 @@ int main()
   string nickName;
   int lucky;
   //get user input
-  cout<<"What is your first name?: ";
+  cout<<"What is your first name?"<<endl;
   cin>>first;
-  cout<<endl<<"What is your last name?: ";
+  cout<<"What is your last name?"<<endl;
   cin>>last;
   
   //tell fortune
@@ -26,13 +26,33 @@ int main()
   nickName += last[0];
   nickName += ".";
 
-  cout<<endl<<"Welcome, "<<nickName<<", here is your fortune..."<<endl;
+  cout<<"Welcome, "<<nickName<<", here is your fortune..."<<endl;
 
 
   lucky = first.length();
 
-  cout<<endl<<"Your lucky number is "<<lucky<<"."<<endl;
+  cout<<"your lucky number is "<<lucky<<endl;
 
+  nickName = first[0];
+
+  if (nickName=="a"||nickName=="A"||nickName=="e"||nickName=="E"||nickName=="i"||nickName=="I"||nickName=="o"||nickName=="O"||nickName=="u"||nickName=="U")
+  {
+    cout<<"you are destined to be famous!"<<endl;
+  }
+
+  else if(nickName!="a"||nickName!="A"||nickName!="e"||nickName!="E"||nickName!="i"||nickName!="I"||nickName!="o"||nickName!="O"||nickName!="u"||nickName!="U")
+  {
+    cout<<"you should keep a low profile."<<endl;
+  }
+  lucky = last.length() - 1;
+  nickName = last[lucky];
+
+  if(nickName=="a"||nickName=="A"||nickName=="e"||nickName=="E"||nickName=="i"||nickName=="I"||nickName=="o"||nickName=="O"||nickName=="u"||nickName=="U")
+  {
+    cout<<"you have already met your true love."<<endl;
+  }
+
+  cout<<"have a good day!"<<endl;
 
   return 0;
 }
